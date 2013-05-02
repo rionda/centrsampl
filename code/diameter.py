4251bf82805a1ae0e90202deeff5caf4fc45b9a5
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-02 17:30:14 -0400

    Add code to compute the diameter and the diameter approximation. Some XXX leftovers where we need to check stuff.

---
+++ code/diameter.py
@@ -0,0 +1,59 @@
+#! /bin/env python3
+
+import argparse
+import logging
+import time
+import igraph as ig
+
+def main():
+    """Compute the diameter of the graph and the time needed to compute it, and
+    write these info as  graph attributes.
+    
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.description = "Compute the diameter of a graph and the time needed to compute it, and (if specified) write these info as a graph attributes"
+    parser.add_argument("graph", help="graph file")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="increase verbosity (use multiple times for more verbosity)")
+    parser.add_argument("-w", "--write", action="store_true", default=False,
+    help="write the diameter of the graph as the 'diameter' graph attribute and the time taken to compute it as the 'diam_time' attribute")
+    args = parser.parse_args()
+
+    # Set the desired level of logging
+    log_format='%(levelname)s: %(message)s'
+    if args.verbose == 1:
+        logging.basicConfig(format=log_format, level=logging.INFO)
+    elif args.verbose >= 2:
+        logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+    # Read graph from file, using auto-detection of format
+    logging.info("Reading graph from %s", args.graph) 
+    G = ig.Graph.Read(args.graph)
+
+    # Compute the diameter
+    logging.info("Computing diameter")
+    # XXX I'm not sure process_time is the right function to use
+    start_time = time.process_time()
+    # XXX What exactly does this compute? Especially for directed graphs
+    diameter = G.diameter() # This is not the vertex-diameter !!! 
+    end_time = time.process_time()
+    elapsed_time = end_time - start_time
+
+    # Print info
+    logging.info("Diameter is %d, computed in %f seconds", diameter,
+            elapsed_time)
+    print("{}, diameter={}, time={}".format(args.graph, diameter,
+        elapsed_time))
+
+    # If requested, add graph attributes and write graph back to original file
+    if args.write:
+        logging.info("Writing diameter and time to graph")
+        G["diameter"] = diameter
+        G["diam_time"] = elapsed_time
+        # We use format auto-detection, which should work given that it worked
+        # when we read the file
+        G.write(args.graph) 
+
+if __name__ == "__main__":
+    main()
+
---
+++ code/diameter_approx.py
@@ -0,0 +1,78 @@
+#! /bin/env python3
+
+import argparse
+import logging
+import random
+import time
+import igraph as ig
+
+def main():
+    """Compute an approximation of diameter of the graph and the time needed to compute it, and
+    write these info as  graph attributes.
+    
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.description = "Compute an approximation of the diameter of a graph and the time needed to compute it, and (if specified) write these info as a graph attributes"
+    parser.add_argument("graph", help="graph file")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="increase verbosity (use multiple times for more verbosity)")
+    parser.add_argument("-w", "--write", action="store_true", default=False,
+    help="write the approximation of diameter of the graph as the 'approx_diameter' graph attribute and the time taken to compute it as the 'approx_diam_time' attribute")
+    args = parser.parse_args()
+
+    # Set the desired level of logging
+    log_format='%(levelname)s: %(message)s'
+    if args.verbose == 1:
+        logging.basicConfig(format=log_format, level=logging.INFO)
+    elif args.verbose >= 2:
+        logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+    # Read graph from file, using auto-detection of format
+    logging.info("Reading graph from %s", args.graph) 
+    G = ig.Graph.Read(args.graph)
+
+    # Check if graph is directed and act accordingly
+    was_directed = False
+    if G.is_directed():
+        logging.warning("Graph is directed, converting it to undirected")
+        G.to_undirected()
+        was_directed = True
+
+    # Seed the random number generator
+    random.seed()
+
+    # Compute the diameter
+    logging.info("Computing diameter")
+    # XXX I'm not sure process_time is the right function to use
+    start_time = time.process_time()
+    # sample a vertex uniformly at random
+    sampled_vertex = G.vs[random.randint(0, len(G.vs)-1)]
+    # We convert the list to a set to remove duplicates
+    shortest_path_lengths = set(G.shortest_paths_dijkstra([sampled_vertex])[0]) - set([float('inf')])
+    diameter = max(shortest_path_lengths)
+    diameter += max(shortest_path_lengths - set([diameter]))
+    end_time = time.process_time()
+    elapsed_time = end_time - start_time
+
+    # Print info
+    logging.info("Diameter approximation is %d, computed in %f seconds", diameter,
+            elapsed_time)
+    print("{}, diameter={}, time={}".format(args.graph, diameter,
+        elapsed_time))
+
+    # If requested, add graph attributes and write graph back to original file
+    if args.write:
+        logging.info("Writing diameter approximation and time to graph")
+        # If the graph was directed and we converted it, re-read it
+        if was_directed:
+            G = ig.Graph.Read(args.graph)
+        G["approx_diameter"] = diameter
+        G["approx_diam_time"] = elapsed_time
+        # We use format auto-detection, which should work given that it worked
+        # when we read the file
+        G.write(args.graph) 
+
+if __name__ == "__main__":
+    main()
+
+
