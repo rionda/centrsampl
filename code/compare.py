37b45df6b8a1a9799fc0841852b6ca87be199eb2
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-03 09:25:57 -0400

    Add skeleton for code to compare results.

---
+++ code/compare.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+"""compare.py
+
+Compare estimations of betweenness centrality to exact values.
+
+"""
+import argparse
+import igraph as ig
+
+def main():
+    """Parse arguments, do the comparison, write to output."""
+    pass
+
+if __name__ == "__main__":
+    main()
+
