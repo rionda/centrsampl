817e052e5739ac72a722550b4ec62c0d4fd654f8
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-03 06:49:09 -0400

    Start implementing Brandes and Pick algorithm for betweenness approximation through sampling.

--- code/brandespich_sample.py
+++ code/brandespich_sample.py
@@ -3,6 +3,10 @@
 import argparse
 import logging
 import math
+import os
+import random
+import sys
+import time
 import igraph as ig
 
 def get_sample_size(epsilon, delta, vertices_num):
@@ -18,8 +22,81 @@ def main():
     in the graph using the algorithm by Brandes and Pich
     http://www.worldscientific.com/doi/abs/10.1142/S0218127407018403  
     """
-    # TODO Implement everything.
-    pass
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.description = "Compute approximate betweenness centrality of all vertices in a graph using the algorihm by Brandes and Pich, and the time to compute them, and write them to file"
+    parser.add_argument("epsilon", type=float, help="graph file")
+    parser.add_argument("delta", type=float, help="graph file")
+    parser.add_argument("graph", help="graph file")
+    parser.add_argument("output", help="output file")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="increase verbosity (use multiple times for more verbosity)")
+    parser.add_argument("-w", "--write", action="store_true", default=False,
+            help="store the approximate betweenness as an attribute of each vertex the graph and the computation time as attribute of the graph, and write these to the graph file")
+
+    args = parser.parse_args()
+
+    # Set the desired level of logging
+    log_format='%(levelname)s: %(message)s'
+    if args.verbose == 1:
+        logging.basicConfig(format=log_format, level=logging.INFO)
+    elif args.verbose >= 2:
+        logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+    # Read graph
+    logging.info("Reading graph from %s", args.graph)
+    try:
+        G = ig.Graph.Read(args.graph)
+    except OSError as E:
+        # XXX There seems to be some problem in the propagation of E.strerror,
+        # so the following actually print None at the end. Not our fault. We
+        # leave it here as perhaps it will be fixed upstream at some point.
+        logging.critical("Cannot read graph file %s: %s", E.strerror)
+        sys.exit(2)
+
+    # Seed the random number generator
+    random.seed()
+
+    # Compute betweenness. We do not use logging from here to the end of the
+    # computation to avoid wasting time (XXX right?)
+    logging.info("Computing betweenness")
+    betweenness = [0] * G.vcount()
+    start_time = time.process_time()
+    sample_size = get_sample_size(args.epsilon, args.delta, G.vcount())
+    for i in range(sample_size):
+        # Sample a vertex uniformly at random
+        sampled_vertex = random.randrange(G.vcount())
+        # get_all_shortest_paths returns a list of shortest paths
+        shortest_paths = G.get_all_shortest_paths(sampled_vertex) 
+        # TODO Update betweenness counters for vertices 
+    end_time = time.process_time()
+    elapsed_time = end_time - start_time
+    logging.info("Betweenness computation complete, took %s seconds",
+            elapsed_time)
+
+    # Denormalize betweenness counters by n / k
+    normalization = G.vcount() / sample_size 
+    betweenness = list(map(lambda x : x * normalization, betweenness))
+
+    # If specified, write betweenness as vertex attributes, and time as graph
+    # attribute
+    if args.write:
+        logging.info("Writing betweenness as vertex attributes and time as graph attribute")
+        G.vs["bp_betw"] = betweenness
+        G["bp_betw_time"] = elapsed_time
+        G.write(args.graph)
+
+    # Write betweenness and time to output
+    try:
+        with open(args.output, 'wt') as output:
+            logging.info("Writing betweenness and time to output file")
+            output.write("({}, {})\n".format(betweenness, elapsed_time))
+    except OSError as E:
+        logging.critical("Cannot write betweenness to %s: %s", args.output,
+                os.strerror(E.errno))
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
