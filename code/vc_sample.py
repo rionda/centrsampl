709379660226e25da8bcf89ddbb33eeb1aa6bd8b
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-04 19:53:53 -0400

    Add homegrown computation of betweenness centrality using vc-dimension.

--- code/vc_sample.py
+++ code/vc_sample.py
@@ -17,7 +17,8 @@ import diameter
 import diameter_approx
 import util
 
-def betweenness(graph, epsilon, delta, use_approx_diameter=True, set_attributes=True):
+def betweenness_homegrown(graph, epsilon, delta, use_approx_diameter=True,
+        set_attributes=True):
     """Compute approximate betweenness using VC-Dimension.
     
     Compute approximations of the betweenness centrality of all the vertices in
@@ -35,13 +36,113 @@ def betweenness(graph, epsilon, delta, use_approx_diameter=True, set_attributes=
     perform any computation for the diameter.
     If set_attributes is True (default), then set the values of the betweenness
     as vertex attributes, and the time as a graph attribute.
+
+    Homegrown version
     
     """
     # We minimize the use of logging from here to the end of the computation to avoid
     # wasting time 
-    logging.info("Computing betweenness")
-    # Seed the random number generator
-    random.seed()
+    logging.info("Computing approximate betweenness using VC-Dimension, homegrown implementation")
+    betw = [0] * graph.vcount()
+    sampled_path = False
+    start_time = time.process_time()
+    # Use desired diameter
+    if int(use_approx_diameter) == 1: # Use approximate diameter
+        diameter_approx.diameter_homegrown(graph)
+        # TODO Update betweenness counters for vertices internal of the sampled path
+        sampled_path = True
+        # Compute VC-dimension upper bound using the approximate diameter
+        vcdim_upp_bound = math.floor(math.log2(graph["approx_diam"] - 1))
+    elif int(use_approx_diameter) == 0: # Use exact diameter
+        diameter.diameter(graph) 
+        vcdim_upp_bound = math.floor(math.log2(graph["diam"] - 1))
+    elif int(use_approx_diameter) > 1: # Use specified diameter
+        # Set the appropriate graph attributes explicitly, given that we don't
+        # call any diameter function
+        graph["vc_diam"] = use_approx_diameter
+        graph["diam_time"] = 0
+        vcdim_upp_bound = math.floor(math.log2(use_approx_diameter -1) + 1)
+        # Set the appropriate graph attributes explicitly, given that we don't call
+    else: # int(use_approx_diameter) < 0
+        logging.critical("got negative diameter explicitly passed. Exiting")
+        sys.exit(2)
+    sample_size = get_sample_size(epsilon, delta, vcdim_upp_bound)
+    # Adjust if we got a sample while computing the approximate diameter
+    if sampled_path:
+        sample_size -= 1
+    for i in range(sample_size):
+        # Sample a pair of different vertices uniformly at random
+        (source, destination) = random.sample(range(graph.vcount()), 2)
+        # Compute all shortest paths between sampled vertices
+        util.compute_shortest_paths_dijkstra(graph, source, destination)
+        # Walk backwards from the destination, sampling a shortest path at
+        # random and updating the betweenness of vertices along this path
+        vertex = destination
+        while True:
+            if len(graph.vs[vertex]["preds"]) > 1:
+                # Recipe for weighted sampling from Python API docs for random
+                weighted_choices = [(x, graph.vs[x]["paths"]) for x in
+                        graph.vs[vertex]["preds"]]
+                population = [val for val, cnt in weighted_choices for i in range(cnt)]
+                sampled_pred = random.choice(population)
+            else:
+                sampled_pred = graph.vs[vertex]["preds"][0]
+            # Update betweenness counter of internal node
+            if sampled_pred != source:
+                betw[sampled_pred] += 1
+            else:
+                break
+    end_time = time.process_time()
+    elapsed_time = end_time - start_time
+    logging.info("Betweenness computation complete, took %s seconds",
+            elapsed_time)
+
+    # Denormalize betweenness counters by (n choose 2) / k
+    normalization = graph.vcount() * (graph.vcount() - 1) / (2 * sample_size)
+    betw = list(map(lambda x : x * normalization, betw))
+
+    # Write attributes to graph, if specified
+    if set_attributes:
+        graph["vc_betw_time"] = elapsed_time
+        graph["vc_delta"] = delta
+        if int(use_approx_diameter) == 1:
+            graph["vc_diam_type"] = "approx" 
+        elif int(use_approx_diameter) == 0:
+            graph["vc_diam_type"] = "exact"
+        else:
+            graph["vc_diam_type"] = "specif"
+        graph["vc_eps"] = epsilon
+        graph["vc_type"] = "homegrown"
+        graph.vs["vc_betw"] = betw
+
+    return (elapsed_time, betw)
+
+def betweenness_igraph(graph, epsilon, delta, use_approx_diameter=True, set_attributes=True):
+    """Compute approximate betweenness using VC-Dimension.
+    
+    Compute approximations of the betweenness centrality of all the vertices in
+    the graph using sampling and the VC-Dimension, and the time needed to
+    compute them. 
+
+    Return a tuple with the time needed to compute the betweenness and the list
+    of betweenness values (one for each vertex in the graph).
+
+    The meaning of the use_approx_diameter parameter is peculiar. If True or
+    1 (default), compute an approximation of the diameter (only valid for
+    undirected, unweighted graphs). If False or 0, compute
+    the exact diameter (which kind of defeat the purpose of sampling, by the
+    way). If any integer > 1, use this value for the diameter, i.e. do not
+    perform any computation for the diameter.
+    If set_attributes is True (default), then set the values of the betweenness
+    as vertex attributes, and the time as a graph attribute.
+
+    igraph version
+    
+    """
+    # We minimize the use of logging from here to the end of the computation to avoid
+    # wasting time 
+    logging.info("Computing approximate betweenness using VC-Dimension, igraph version")
+
     betw = [0] * graph.vcount()
     sampled_path = False
     start_time = time.process_time()
@@ -60,7 +161,7 @@ def betweenness(graph, epsilon, delta, use_approx_diameter=True, set_attributes=
     elif int(use_approx_diameter) > 1: # Use specified diameter
         # Set the appropriate graph attributes explicitly, given that we don't
         # call any diameter function
-        graph["diam"] = use_approx_diameter
+        graph["vc_diam"] = use_approx_diameter
         graph["diam_time"] = 0
         vcdim_upp_bound = math.floor(math.log2(use_approx_diameter -1) + 1)
         # Set the appropriate graph attributes explicitly, given that we don't call
@@ -98,16 +199,30 @@ def betweenness(graph, epsilon, delta, use_approx_diameter=True, set_attributes=
     if set_attributes:
         graph["vc_betw_time"] = elapsed_time
         graph["vc_delta"] = delta
-        graph["vc_eps"] = epsilon
         if int(use_approx_diameter) == 1:
-            graph["vc_type"] = "approx" 
+            graph["vc_diam_type"] = "approx" 
         elif int(use_approx_diameter) == 0:
-            graph["vc_type"] = "exact"
+            graph["vc_diam_type"] = "exact"
         else:
-            graph["vc_type"] = "specif"
+            graph["vc_diam_type"] = "specif"
+        graph["vc_eps"] = epsilon
+        graph["vc_type"] = "igraph"
         graph.vs["vc_betw"] = betw
 
     return (elapsed_time, betw)
+
+def betweenness(graph, epsilon, delta, use_approx_diameter=True,
+        implementation="igraph",  set_attributes=True):
+    if implementation == "igraph":
+        return betweenness_igraph(graph, epsilon, delta, use_approx_diameter,
+                set_attributes)
+    elif implementation == "homegrown":
+        return betweenness_homegrown(graph, epsilon, delta,
+                use_approx_diameter, set_attributes)
+    else:
+        logging.critical("Betweenness implementation not recognized: %s",
+                implementation)
+        sys.exit(2)
 
 def get_sample_size(epsilon, delta, vcdim_upper_bound, c=0.5):
     """Compute sample size.
@@ -139,6 +254,9 @@ def main():
             help="value to use for the diameter")
     group.add_argument("-e", "--exact", action="store_true", default=False,
             help="use exact diameter (default)")
+    parser.add_argument("-i", "--implementation", choices=["igraph",
+        "homegrown"], default="igraph", 
+        help="use specified implementation of betweenness computation")
     parser.add_argument("-v", "--verbose", action="count", default=0,
             help="increase verbosity (use multiple times for more verbosity)")
     parser.add_argument("-w", "--write", action="store_true", default=False,
@@ -149,16 +267,19 @@ def main():
     # Set the desired level of logging
     util.set_verbosity(args.verbose)
 
+    # Seed the random number generator
+    random.seed()
+
     # Read graph
     G = util.read_graph(args.graph)
 
     # Compute betweenness
     if args.diameter > 0:
-        (elapsed_time, betw) = betweenness(args.graph, args.epsilon,
-                args.delta, args.diameter, True)
+        (elapsed_time, betw) = betweenness(G, args.epsilon,
+                args.delta, args.diameter, args.implementation, True)
     else:
-        (elapsed_time, betw) = betweenness(args.graph, args.epsilon,
-                args.delta, args.approximate, True)
+        (elapsed_time, betw) = betweenness(G, args.epsilon,
+                args.delta, args.approximate, args.implementation, True)
 
     # If specified, write betweenness as vertex attributes, and time as graph
     # attribute back to file
