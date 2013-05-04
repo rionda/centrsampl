b976b6c4e76d4d12888e8869e90d8ec4367e54f6
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-04 14:19:10 -0400

    Add implementation of Dijkstra's shortest path algorithm that takes into account the possibility of multiple shortest paths. Untested.

--- code/util.py
+++ code/util.py
@@ -3,10 +3,71 @@
 Various useful functions.
 """
 import argparse
+import heapq
 import logging
 import sys
 import igraph as ig
 
+def get_all_shortest_paths_dijkstra(graph, source, to=None, weights=None):
+    """Compute all shortest paths from a given source in the graph.
+    
+    Compute the shortest paths from source to all vertices in the graph or, if
+    "to" is not None, to the specified vertex. "weights" can be edge weights in
+    a list or the name of an edge attribute holding edge weights. If None, all
+    edges are assumed to have unitary weight.
+
+    Return a list of vertices sorted by non-increasing distance from the
+    source.
+    """  
+    # Initialization
+    # list of predecessors
+    graph.vs["preds"] = [] * graph.vcount()
+    # number of shortest paths through this node
+    graph.vs["paths"] = [0] * graph.vcount ()
+    graph.vs[source]["paths"] = 1
+    # current distance from the source
+    graph.vs["dist"] = [-1] * graph.vcount()
+    graph.vs[source]["dist"] = 0
+    # After the shortest paths computation, this stack can be used to return
+    # vertices in order of non-increasing distance from the source. Useful for
+    # betweenness computation.
+    vertices_stack = []
+    # distance-from-source min-proprity queue
+    distance_heap = []
+    # Take care of weights
+    if weights == None:
+        _weights = [1] * graph.ecount()
+    else: 
+        if weights in graph.edge_attributes():
+            _weights = graph.es[weights]
+        else:
+            assert len(weights) == graph.ecount()
+            _weights = weights
+
+    heapq.heappush(distance_heap, (graph.vs[source]["dist"], graph.vs[source]))
+
+    # Shortest paths computation 
+    while distance_heap:
+        vertex = heapq.heappop(distance_heap)
+        vertices_stack.append(vertex)
+        for neighbor in graph.neighbors(vertex):
+            # Relax
+            distance_from_vertex = _weights[graph.eid(vertex, neighbor)]
+            # Is it the first time we see neighbor?
+            if neighbor["dist"] == -1:
+                # Update distance of neighbor from source
+                neighbor["dist"] = vertex["dist"] + distance_from_vertex
+                # Push neighbor to distance_heap
+                heapq.heappush(distance_heap, (neighbor["dist"], neighbor))
+            # shortest path to neighbor via vertex?
+            if neighbor["dist"] == vertex["dist"] + distance_from_vertex:
+                # Update number of paths from source to neighbor
+                neighbor["paths"] += vertex["paths"]
+                # Add vertex to the predecessors of neightbor
+                neighbor["preds"].append(vertex)
+    # Return the stack of vertices
+    return vertices_stack
+  
 def positive_int(string):
     """Check validity of string as positive integer. Return value if it is.
     
