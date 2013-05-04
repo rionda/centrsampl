51073f2e096c6c7646be80c727a5c47f3e7ae5d0
Matteo Riondato <matteo_riondato@brown.edu>
2013-05-04 18:49:36 -0400

    Add homegrown version of betweenness computation with Brandes and Pich algorithm

--- code/brandespich_sample.py
+++ code/brandespich_sample.py
@@ -4,20 +4,21 @@
 Compute approximations of the betweenness centrality of all the vertices
 in the graph using the algorithm by Brandes and Pich, and the time needed to
 compute them. These values are then written to an output file. For the
-algorihm, see http://www.worldscientific.com/doi/abs/10.1142/S0218127407018403 .
+algorithm, see http://www.worldscientific.com/doi/abs/10.1142/S0218127407018403 .
 
 """
 import argparse
 import itertools
 import logging
 import math
-import os
 import random
+import sys
 import time
 
+import brandes_exact
 import util
 
-def betweenness(graph, epsilon, delta, set_attributes=True):
+def betweenness_homegrown(graph, epsilon, delta, set_attributes=True):
     """Compute approx. betweenness using Brandes and Pick algorithm.
     
     Compute approximations of the betweenness centrality of all the vertices in
@@ -30,10 +31,60 @@ def betweenness(graph, epsilon, delta, set_attributes=True):
     If set_attributes is True (default), then set the values of the betweenness
     as vertex attributes, and the time as a graph attribute.
     
+    Homegrown version
     """
     # We do not use logging from here to the end of the computation to avoid
     # wasting time
-    logging.info("Computing betweenness")
+    logging.info("Computing approximate betweenness using Brandes and Pich algorithm, homegrown implementation")
+    # Seed the random number generator
+    random.seed()
+    betw = [0] * graph.vcount()
+    start_time = time.process_time()
+    sample_size = get_sample_size(epsilon, delta, graph.vcount())
+    for i in range(sample_size):
+        # Sample a source vertex uniformly at random
+        sampled_source_index = random.randrange(graph.vcount())
+        sampled_source = graph.vs[sampled_source_index]
+        # Update betweenness with the contribution of this source
+        betw = brandes_exact.update_betweenness(graph, sampled_source, betw)
+    end_time = time.process_time()
+    elapsed_time = end_time - start_time
+    logging.info("Betweenness computation complete, took %s seconds",
+            elapsed_time)
+
+    # Denormalize betweenness counters by n / k
+    normalization = graph.vcount() / sample_size 
+    betw = list(map(lambda x : x * normalization, betw))
+
+    # Write attributes to graph, if specified
+    if set_attributes:
+        graph["bp_betw_time"] = elapsed_time
+        graph["bp_delta"] = delta
+        graph["bp_eps"] = epsilon
+        graph["bp_type"] = "homegrown"
+        graph.vs["bp_betw"] = betw
+
+    return (elapsed_time, betw)
+
+def betweenness_igraph(graph, epsilon, delta, set_attributes=True):
+    """Compute approx. betweenness using Brandes and Pick algorithm.
+    
+    Compute approximations of the betweenness centrality of all the vertices in
+    the graph using the algorithm by Brandes and Pich, and the time needed to
+    compute them. For the algorihm, see
+    http://www.worldscientific.com/doi/abs/10.1142/S0218127407018403 .
+
+    Return a tuple with the time needed to compute the betweenness and the list
+    of betweenness values (one for each vertex in the graph).
+    If set_attributes is True (default), then set the values of the betweenness
+    as vertex attributes, and the time as a graph attribute.
+
+    igraph version
+
+    """
+    # We do not use logging from here to the end of the computation to avoid
+    # wasting time
+    logging.info("Computing approximate betweenness using Brandes and Pich algorithm, igraph implementation")
     # Seed the random number generator
     random.seed()
     betw = [0] * graph.vcount()
@@ -69,9 +120,31 @@ def betweenness(graph, epsilon, delta, set_attributes=True):
         graph["bp_betw_time"] = elapsed_time
         graph["bp_delta"] = delta
         graph["bp_eps"] = epsilon
+        graph["bp_type"] = "igraph"
         graph.vs["bp_betw"] = betw
 
     return (elapsed_time, betw)
+
+def betweenness(graph, epsilon, delta, implementation="igraph",
+        set_attributes=True):
+    """Compute approx. betweenness using Brandes and Pick algorithm.
+
+    Return a tuple with the time needed to compute the betweenness and the list
+    of betweenness values (one for each vertex in the graph).
+    Compute betweenness using the specified implementation (available: igraph,
+    homegrown).
+    If set_attributes is True (default), then set the values of the betweenness
+    as vertex attributes, and the time as a graph attribute.
+    
+    """
+    if implementation == "igraph":
+        return betweenness_igraph(graph, epsilon, delta, set_attributes)
+    elif implementation == "homegrown":
+        return betweenness_homegrown(graph, epsilon, delta, set_attributes)
+    else:
+        logging.critical("Betweenness implementation not recognized: %s",
+                implementation)
+        sys.exit(2)
 
 def get_sample_size(epsilon, delta, vertices_num):
     """Compute sample size.
@@ -97,6 +170,9 @@ def main():
             help="confidence parameter")
     parser.add_argument("graph", help="graph file")
     parser.add_argument("output", help="output file")
+    parser.add_argument("-i", "--implementation", choices=["igraph",
+        "homegrown"], default="igraph", 
+        help="use specified implementation of betweenness computation")
     parser.add_argument("-v", "--verbose", action="count", default=0, help="increase verbosity (use multiple times for more verbosity)")
     parser.add_argument("-w", "--write", action="store_true", default=False,
             help="store the approximate betweenness as an attribute of each vertex the graph and the computation time as attribute of the graph, and write these to the graph file")
@@ -110,7 +186,7 @@ def main():
     G = util.read_graph(args.graph)
 
     # Compute betweenness
-    (elapsed_time, betw) = betweenness(G, args.epsilon, args.delta, True)
+    (elapsed_time, betw) = betweenness(G, args.epsilon, args.delta, args.implementation, True)
 
     # If specified, write betweenness as vertex attributes, and time as graph
     # attribute back to file
