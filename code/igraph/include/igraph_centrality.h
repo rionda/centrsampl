a24111ac03b00ac0cb875cc90c2d746e518ac6d1
kornarop <evgenios_kornaropoulos@brown.edu>
2013-05-15 16:19:12 -0300

    Update igraph_centrality.h
    added motwani's approx

--- code/igraph/include/igraph_centrality.h
+++ code/igraph/include/igraph_centrality.h
@@ -93,6 +93,7 @@ int igraph_betweenness_sample_vc_sample_size(const igraph_t *graph,
                                              igraph_real_t cutoff, 
                                              const igraph_vector_t* weights,
                                              igraph_bool_t nobigint);
+int igraph_diameter_approximation_motwani(const igraph_t *graph, igraph_integer_t *diameter );                                             
 // XXX TODO THIS REALLY DOES NOT BELONG HERE
 int igraph_diameter_approximation(const igraph_t *graph, igraph_integer_t *res,
                                   igraph_vector_t *stats, igraph_strvector_t *stats_names,
